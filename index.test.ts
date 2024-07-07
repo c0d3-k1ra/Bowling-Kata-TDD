b13949628c6adf33b1dd7b95f756282dc2095b24
c0d3-k1ra <amit.upadhyay021@outlook.com>
2024-07-07 22:59:35 +0530

    1 Spare

--- index.test.ts
+++ index.test.ts
@@ -1,30 +1,36 @@
 import BowlingGame from './index';
 
+const rollContinouslyFor = (game: BowlingGame, value: number, times: number) => {
+  for (let i = 0; i < times; i++) {
+    game.roll(value);
+  }
+};
 
+const rollASpare = (game: BowlingGame) => {
+  rollContinouslyFor(game, 5, 2);
+}
 
 describe("Bowling Kata", () => {
-  let game = new BowlingGame();
-
-  const rollContinouslyFor = (value: number, times: number) => {
-    for (let i = 0; i < times; i++) {
-      game.roll(value);
-    }
-  };
-
-  beforeEach = () => {
-    game = new BowlingGame();
-  };
-  
   test('Gutter game should score 0', () => {
-    rollContinouslyFor(0, 20);
+    const game = new BowlingGame();
+    rollContinouslyFor(game, 0, 20);
   
     expect(game.getScore()).toBe(0);
   });
   
   test('All rolls are 1s', () => {
-    rollContinouslyFor(1, 20);
+    const game = new BowlingGame();
+    rollContinouslyFor(game, 1, 20);
 
     expect(game.getScore()).toBe(20);
+  });
+
+  test('1 spare', () => {
+    const game = new BowlingGame();
+    rollASpare(game);
+    rollContinouslyFor(game, 1,18);
+    
+    expect(game.getScore()).toBe(29);
   });
 });
 
--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import { isSpreadElement } from "typescript";
+
 class BowlingGame {
   private readonly rolls: number[] = [];
   private currentRoll: number = 0;
@@ -8,10 +10,24 @@ class BowlingGame {
 
   getScore(): number {
     let score = 0;
-    for (let roll = 0; roll < this.currentRoll; roll++) {
-      score += this.rolls[roll];
+    let rollIndex = 0;
+    for (let frame = 0; frame < 10; frame++) {
+      if(this.isSpare(rollIndex)) {
+        score += 10 + this.bonusForSpare(rollIndex);
+      } else {
+        score += this.rolls[rollIndex] + this.rolls[rollIndex+1]
+      }
+      rollIndex += 2;
     }
     return score;
+  }
+
+  private isSpare(rollIndex: number): boolean {
+    return this.rolls[rollIndex] + this.rolls[rollIndex + 1] == 10;
+  }
+
+  private bonusForSpare(rollIndex: number): number {
+    return this.rolls[rollIndex + 2];
   }
 }
 
